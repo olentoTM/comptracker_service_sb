88f8590fc3c04f6ca658ae66cab257a0d8693f01
Markus Jäntti <jantti.markus@hotmail.fi>
2024-08-10 15:03:54 +0300

    Initial commit with the new project template

---
+++ src/main/java/fi/olentotm/comptracker_service/ComptrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package fi.olentotm.comptracker_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComptrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComptrackerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/olentotm/comptracker_service/ComptrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.olentotm.comptracker_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComptrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
